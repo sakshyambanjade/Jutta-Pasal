7b7da379010dabcba49ba335aadf3d868e253a6b
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2023-06-17 21:00:08 +0545

    added hl component

---
+++ frontend/frontend/src/Components/HorizontalLine/Hl.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export  function Hl() {
+  return (
+    <div>
+            <hr class="h-px my-8 bg-gray-200 border-0 dark:bg-gray-100"/>
+    </div>
+  )
+}
--- frontend/frontend/src/Pages/Home/Home.js
+++ frontend/frontend/src/Pages/Home/Home.js
@@ -2,16 +2,15 @@ import React, { Component } from 'react'
 import {Navbar} from '../../Components/Navbar/Navbar'
 import {Footer} from '../../Components/Footer/Footer'
 import {Sidebar} from '../../Components/SideMenu/SideMenu'
-
+import {Hl} from '../../Components/HorizontalLine/Hl'
 export default class Home extends Component {
   render() {
     return (
       <>
      <div className='Navigation Bar'><Navbar/></div>
+     <Hl/>
       <div className='Menu-Side-Bar'>  <Sidebar/></div>
      <div className='Footer-Section'><Footer/></div>
-
-   
       </>
     )
   }
