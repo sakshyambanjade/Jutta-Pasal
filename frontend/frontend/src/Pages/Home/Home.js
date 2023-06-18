6e4b8f14eaf8d38e248624df445e620e647682ac
SAKSHYAM BANJADE <sakshyambanjade@gmail.com>
2023-06-18 20:19:31 +0545

    added cart

--- frontend/frontend/src/Pages/Home/Home.js
+++ frontend/frontend/src/Pages/Home/Home.js
@@ -3,14 +3,19 @@ import {Navbar} from '../../Components/Navbar/Navbar'
 import {Footer} from '../../Components/Footer/Footer'
 import {Sidebar} from '../../Components/SideMenu/SideMenu'
 import {Hl} from '../../Components/HorizontalLine/Hl'
+import {Cart} from '../../Components/Cart/Cart'
 export default class Home extends Component {
   render() {
     return (
       <>
      <div className='Navigation Bar'><Navbar/></div>
      <Hl/>
-      <div className='Menu-Side-Bar'>  <Sidebar/></div>
+     <div className='Menu-Side-Bar'>  <Sidebar/></div>
+
+     <div className='Cart-Section'><Cart/></div>
+
      <div className='Footer-Section'><Footer/></div>
+
       </>
     )
   }
